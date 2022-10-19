9eb5b9f1ab7311eb816ce8164b52c5e8b6ad8914
zhuyangyang <yangyang.zhu@pplingo.com>
2022-10-19 14:00:59 +0800

    [Feat] first init
    Signed-off-by: zhuyangyang <yangyang.zhu@pplingo.com>

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: TextButton(
+        onPressed: () {},
+        child: Padding(
+          padding: const EdgeInsets.only(
+              left: 0.0, top: 10.0, right: 10.0, bottom: 10.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                child: const Text(
+                  '这是一点描述',
+                  style: TextStyle(color: Colors.blue, fontSize: 14.0),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                margin: const EdgeInsets.only(top: 6.0, bottom: 2.0),
+                alignment: Alignment.topLeft,
+              ),
+              Image.asset('assets/images/pad/booking/github.png'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
